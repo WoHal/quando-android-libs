0d0b27460e30d14f7caae95331eb58d1f4c71c6e
WoHal <socianerr@gmail.com>
2026-01-07 20:18:44 +0800

    publish to maven

--- preferencestore/build.gradle.kts
+++ preferencestore/build.gradle.kts
@@ -4,9 +4,10 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-//    alias(libs.plugins.maven.publish)
-    id("maven-publish")
+    alias(libs.plugins.maven.publish)
 }
+
+val libVersion = "0.0.1"
 
 android {
     namespace = "wang.soian.preferencestore"
@@ -37,13 +38,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
-//            withJavadocJar()
-        }
-    }
 }
 
 kotlin {
@@ -61,63 +55,39 @@ dependencies {
 }
 
 afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                from(components["release"])
-                groupId = "wang.soian.quando"
-                artifactId = "preferencestore"
-                version = "0.0.1"
-            }
-        }
+    mavenPublishing {
+        publishToMavenCentral(true)
+        signAllPublications()
+        coordinates(
+            "io.github.wang",
+            "quando-preference",
+            libVersion
+        )
 
-        repositories {
-            maven {
-                name = "GithubPackages"
-                url = uri("https://maven.pkg.github.com/WoHal/quando-android")
-                credentials {
-                    username = project.findProperty("gpr.user") as? String ?: System.getenv("USERNAME")
-                    password = project.findProperty("gpr.key") as? String ?: System.getenv("ACCESS_TOKEN")
+        pom {
+            name = "Android shared preferences"
+            description = "The android shared preferences library."
+            inceptionYear = "2025"
+            url = "https://github.com/WoHal/quando-android/"
+            licenses {
+                license {
+                    name = "MIT License"
+                    url = "https://opensource.org/license/mit/"
+                    distribution = "repo"
                 }
+            }
+            developers {
+                developer {
+                    id = "WoHal"
+                    name = "WoHal"
+                    url = "https://github.com/WoHal/"
+                }
+            }
+            scm {
+                url = "https://github.com/WoHal/quando-android/"
+                connection = "scm:git:git://github.com/WoHal/quando-android.git"
+                developerConnection = "scm:git:ssh://git@github.com/WoHal/quando-android.git"
             }
         }
     }
 }
-
-//afterEvaluate {
-//    mavenPublishing {
-//        publishToMavenCentral(true)
-//        signAllPublications()
-//        coordinates(
-//            "wang.soian.quando",
-//            "preferences-lib",
-//            version as String?
-//        )
-//
-//        pom {
-//            name = "Android shared preferences"
-//            description = "The android shared preferences library."
-//            inceptionYear = "2025"
-//            url = "https://github.com/WoHal/quando-android/"
-//            licenses {
-//                license {
-//                    name = "MIT License"
-//                    url = "https://opensource.org/license/mit/"
-//                    distribution = "repo"
-//                }
-//            }
-//            developers {
-//                developer {
-//                    id = "WoHal"
-//                    name = "WoHal"
-//                    url = "https://github.com/WoHal/"
-//                }
-//            }
-//            scm {
-//                url = "https://github.com/WoHal/quando-android/"
-//                connection = "scm:git:git://github.com/WoHal/quando-android.git"
-//                developerConnection = "scm:git:ssh://git@github.com/WoHal/quando-android.git"
-//            }
-//        }
-//    }
-//}
